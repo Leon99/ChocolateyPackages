d696092e2b3e23d24625bbb1206cea0d4be9cf5d
Leon V <leon99@outlook.com>
2016-01-25 11:26:15 +1100

    Initial commit

---
+++ rdpwrapper/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName= 'rdpwrapper'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/binarymaster/rdpwrap/releases/download/v1.6/RDPWrap-v1.6.zip' # download url
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+
+Start-ChocolateyProcessAsAdmin '-i' (Join-Path $toolsDir 'RDPWInst.exe') -validExitCodes @(0,1)
+Start-ChocolateyProcessAsAdmin '-w' (Join-Path $toolsDir 'RDPWInst.exe')
---
+++ rdpwrapper/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName= 'rdpwrapper'
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Start-ChocolateyProcessAsAdmin '-u' (Join-Path $toolsDir 'RDPWInst.exe')
+
+Uninstall-ChocolateyZipPackage $packageName 'RDPWrap-v1.6.zip'
