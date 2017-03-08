50c3d2f4fbf20ef42d34daacfb40277ad16e04d6
Leon V <leon99@outlook.com>
2017-03-08 11:49:00 +1100

    Seq package added

---
+++ seq/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'seq'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://getseq.net/Download/Begin?version=3.4.20'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url
+
+  softwareName  = 'Seq'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+& "C:\Program Files\Seq\seq.exe" install
+& "C:\Program Files\Seq\seq.exe" start
+netsh advfirewall firewall add rule name="Open Seq Port 5341" dir=in action=allow protocol=TCP localport=5341
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
