56a7b244dadc15d7831a76dd539c2a3472f5e6c0
Smart123s <28480228+Smart123s@users.noreply.github.com>
2021-12-03 19:54:26 +0100

    Create 'rdpwrap.ini' auto update task
    Original idea from
    https://github.com/asmtron/rdpwrap/raw/master/autoupdate.zip

--- rdpwrapper/tools/chocolateyinstall.ps1
+++ rdpwrapper/tools/chocolateyinstall.ps1
@@ -4,7 +4,16 @@ $packageName= 'rdpwrapper'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/tmaguire/rdpwrap/releases/download/v1.6.3/RDPWrapper-1.6.3.zip'
 $checkSum   = '8d3c945ee817f0c8d8d1ee4de92db6e12b75bf165ef01a847459b5b5f8d7b0d3'
+$PackageParameters = Get-PackageParameters
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir -ChecksumType "sha256" -Checksum $checkSum
 
 Start-ChocolateyProcessAsAdmin '-i -o' (Join-Path $toolsDir 'RDPWInst.exe') -validExitCodes @(0,1)
+
+# Create 'rdpwrap.ini' auto update task
+if (-not ($PackageParameters.NoUpdateService)) {
+    Write-Host "Schedule auto update of 'rdpwrap.ini' on startup."
+    schtasks /create /f /sc ONSTART /tn "RDP Wrapper Autoupdate" /tr "$toolsDir\RDPWInst.exe -w" /ru SYSTEM /delay 0000:10
+    $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
+    Set-ScheduledTask -TaskName 'RDP Wrapper Autoupdate' -Settings $settings
+}
--- rdpwrapper/tools/chocolateyuninstall.ps1
+++ rdpwrapper/tools/chocolateyuninstall.ps1
@@ -4,3 +4,6 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Start-ChocolateyProcessAsAdmin '-u' (Join-Path $toolsDir 'RDPWInst.exe')
 
 Uninstall-ChocolateyZipPackage $packageName 'RDPWrap-v1.6.3.zip'
+
+# Remove 'rdpwrap.ini' auto update task
+schtasks /delete /f /tn "RDP Wrapper Autoupdate"
